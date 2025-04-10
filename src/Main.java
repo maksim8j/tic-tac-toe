798815ec2e1a1d1236a2fb7300099880614f08dd
maksim8j <maksim8r@gmail.com>
2025-04-10 14:30:59 +0300

    chore: establish base methods structure for project organization

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    private static void startGameRound(){
+
+    }
+
+    private static void startGameLoop(){
+
+    }
+
+    private static void makePlayerTurn(){
+
+    }
+
+    private static void makeBotTurn(){
+
+    }
+
+    private static void checkGameState(){
+
+    }
+}
