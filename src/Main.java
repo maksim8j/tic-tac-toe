166bc4ff3c5207fce54d6952bbaf4f61031319fc
maksim8j <maksim8r@gmail.com>
2025-04-10 14:49:28 +0300

    feat: implement createBoard() function for board initialization

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 
     private static void startGameRound(){
 
+    }
+
+    private static String[][] createBoard(){
+        for (int col = 0; col < 3; col++) {
+            for (int row = 0; row < 3; row++) {
+
+            }
+            
+        }
     }
 
     private static void startGameLoop(){
